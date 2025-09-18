a76bfa0221a512c7a96d3c463e27e5dc46c2a7ba
Jay Tandia <jaytandia50@kgpian.iitkgp.ac.in>
2025-09-19 03:01:29 +0530

    create userRouter

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
-import express, { type NextFunction, type Request, type Response } from 'express';
-import globalErrorHandler from './middlewares/globalErrorHandler.js';
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler.js";
+import userRouter from "./user/userRouter.js";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
-    res.json({
-        message: "Welcome to elib apis"
-    })
-})
+  res.json({
+    message: "Welcome to elib apis",
+  });
+});
+
+app.use("/api/users", userRouter)
 
 // Global error handler (provided in express)
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
