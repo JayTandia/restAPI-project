94092a2e3dd13fc6c1a681772647416d7fac3627
Jay Tandia <jaytandia50@kgpian.iitkgp.ac.in>
2025-09-22 22:26:45 +0530

    add book model

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, {
 } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 import userRouter from "./user/userRouter.js";
+import bookRouter from "./book/bookRouter.js";
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/api/users", userRouter)
+
+app.use("api/books", bookRouter)
 
 // Global error handler (provided in express)
 app.use(globalErrorHandler);
---
+++ src/book/bookController.ts
@@ -0,0 +1,7 @@
+import type { NextFunction, Request, Response } from "express";
+
+const createBook = (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+export { createBook }
