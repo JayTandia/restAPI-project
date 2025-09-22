f2e7b5a76985f4e1dd7123af1e591da6ff1305a9
Jay Tandia <jaytandia50@kgpian.iitkgp.ac.in>
2025-09-22 21:58:04 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -48,7 +48,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     });
 
     // response
-    res.json({
+    res.status(201).json({
       accessToken: token,
     });
   } catch (error) {
@@ -56,4 +56,31 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+
+    if(!email || !password){
+        return next(createHttpError(400, "All fields are required"))
+    }
+
+    const user = await userModel.findOne({email})
+
+    if(!user){
+        return next(createHttpError(404, "User not found"))
+    } 
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch){
+        return next(createHttpError(400, "Username or password incorrect"))
+    }
+
+    // create access token
+    const token = jwt.sign({sub: user._id}, config.jwtSecret as string, {expiresIn: '7d'})
+
+    res.json({
+        accessToken: token
+    })
+}
+
+export { createUser, loginUser };
