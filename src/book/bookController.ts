1d894633c772ea48fcd4f35aa3f4978936bbabc1
Jay Tandia <jaytandia50@kgpian.iitkgp.ac.in>
2025-09-23 23:30:09 +0530

    add single book endpoint

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -156,8 +156,23 @@ const listBooks = async (req: Request, res: Response, next: NextFunction) => {
     const book = await bookModel.find()
     res.json(book)
   } catch (error) {
+    return next(createHttpError(500, "Error while getting books"))
+  }
+}
+
+const getSingleBook = async (req: Request, res: Response, next: NextFunction) => {
+
+  const bookId = req.params.bookId
+  try {
+    const book = await bookModel.findOne({_id: bookId})
+    if(!book){
+      return next(createHttpError(404, "Book not found"))
+    }
+
+    return res.json(book)
+  } catch (error) {
     return next(createHttpError(500, "Error while getting a book"))
   }
 }
 
-export { createBook, updateBook, listBooks };
+export { createBook, updateBook, listBooks, getSingleBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook, listBooks, updateBook } from "./bookController.js";
+import { createBook, getSingleBook, listBooks, updateBook } from "./bookController.js";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate.js";
@@ -33,5 +33,7 @@ bookRouter.patch(
 );
 
 bookRouter.get("/", listBooks)
+
+bookRouter.get("/:bookId", getSingleBook)
 
 export default bookRouter;
