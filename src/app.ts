0019a92ff009e8b304075e853bca6cf9b872761d
Jay Tandia <jaytandia50@kgpian.iitkgp.ac.in>
2025-09-24 00:08:26 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,18 @@ import express, {
   type Request,
   type Response,
 } from "express";
+import cors from "cors"
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 import userRouter from "./user/userRouter.js";
 import bookRouter from "./book/bookRouter.js";
+import { config } from "./config/config.js";
 
 const app = express();
 
 app.use(express.json())
+app.use(cors({
+  origin: config.frontendDomain,
+}))
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json({
